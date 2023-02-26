48e11595b81d7d13f23474130ecf61d04dd6450a
Lucas <48070334+LucasRocha-01@users.noreply.github.com>
2023-02-26 00:58:28 -0300

    First Commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@components/Header';
+import { Container } from './style';
+
+export function Groups() {
+  return (
+    <Container >
+      <Header />
+    </Container>
+  );
+}
+
+
